93b557ea1c2ae17daa1952b286020bc4ccbd92bf
Agustin <ajuarez@activo.uy>
2019-01-04 12:25:01 -0300

    ForumVue

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import ThreadShow from '@/components/ThreadShow'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/thread',
+      component: Home,
+      name: 'Home'
+    },
+    {
+      path: '/thread/:id',
+      component: ThreadShow,
+      name: 'ThreadShow',
+      props: {default: true}
+    }
+  ],
+  mode: 'history'
+})
